a9b6bf45126f6d72049c004fd3dbd9e5f8aa1926
coffee247 <jimstallings2020@gmail.com>
2020-07-12 17:21:06 -0400

    Added some color to command-line info in Start.sh

--- Start.sh
+++ Start.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-
+set -e
 up="docker-compose up -d"
 echo ""
 echo "****** Stop any running servers on same port before starting up *****"
 sh ./Stop.sh
 echo "Executing Start command: " $up
 $up
-echo "Now open a browser and navigate to localhost:8085"
+echo ""
+echo -e "Right click ---> \e[34m http://localhost:8085 \e[0m (Launches browser page for adminer web interface)"
+echo ""
 echo "Select PostgreSQL from system dropdown"
-echo "Enter User: postgres"
-echo "Enter Password: password"
+echo -e "Enter User: \e[91m postgres \e[0m"
+echo -e "Enter Password: \e[91m password \e[0m"
 echo ""
-echo "Examine created database called   rbims   "
+echo -e "Examine created database called \e[91m rbims  \e[0m"
 echo ""
-echo "Right click URL --> https://github.com/coffee247 <--  and then FOLLOW on gitHub." 
+echo -e "Right click --> \e[34m https://github.com/coffee247 \e[0m to visit and FOLLOW author on gitHub." 
 echo ""
